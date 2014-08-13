913c714faae640af3ed6460a14427c4308fb46b2
DoriPS13 <palmer.sanderson@gmail.com>
2014-08-12 21:50:52 -0700

    working with live site, no bonus or testing

--- scrapper.rb
+++ scrapper.rb
@@ -1,36 +1,32 @@
 # scrapper.rb
 require 'nokogiri'
 require 'open-uri'
+require 'awesome_print'
+#why does this need to be in this order? I put search on the
+#top and it wouldn't work.
 
-
-
-def filter_links(rows, regex)
-  # takes in rows and returns uses
-  # regex to only return links 
-  # that have "pup", "puppy", or "dog"
-  # keywords
+def filter_links(list)
+  dogs_found = list.select do |results|
+    results.text.match(/dog|Dog|pup|Pup|puppies|Puppies/)
+  end
 end
 
 def get_todays_rows(doc, date_str)
-  #  1.) open chrome console to look in inside p.row to see
-  #  if there is some internal date related content
-
-  #  2.) figure out the class that you'll need to select the
-  #   date from a row
-
+  list = doc.css(".row").select do |results|
+    results.css(".date").text == date_str
+  end
+  filter_links(list)
 end
 
-def get_page_results
-  url = "http://sfbay.craigslist.org/sfc/pet/"
-  Nokogiri::HTML(open(url))
+def get_page_results(date_str)
+  url = open("http://sfbay.craigslist.org/search/sfc/pet?hasPic=1").read
+  page = Nokogiri::HTML(url)
+  get_todays_rows(page,date_str)
 end
 
 def search(date_str)
-  get_page_results
+  get_page_results(date_str)
 end
 
-# want to learn more about 
-# Time in ruby??
-#   http://www.ruby-doc.org/stdlib-1.9.3/libdoc/date/rdoc/Date.html#strftime-method
 today = Time.now.strftime("%b %d")
-search(today)
+ap search(today)
